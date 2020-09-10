477c6751474fe64e0bdfe67127c3ee0f47e22d9c
yutaro78 <tarokun0708@gmail.com>
2020-09-10 21:24:22 +0900

    change3

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -43,7 +43,6 @@ $(function(){
   let reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     let last_message_id = $('.messages-path:last').data("message-id") || 0;
-    console.log(last_message_id)
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
